923f3d9c28947eee5844f43b35c7f79f64a29b7d
Rebecca McGirr <you@example.com>
2025-05-27 10:27:25 -0700

    completed setup for index, tests, and JS6WA09

---
+++ JS6WA09.js
@@ -0,0 +1,14 @@
+// Coding Steps:
+// • For the final project, you will be creating an automated version of the classic card game War! There are many versions of the game War. In this version, there are only 2 players.
+//      o You don’t need to do anything special when there is a tie in a round.
+// • Think about how you would build this project and write your plan down. Consider classes such as Card, Deck, Player, as well as what properties and methods they may include.
+//      Four suits to represent the appearance (user interface - ui) for your cards:
+//      let cardSuits = ["Spades 🗡️", "Hearts ❤️", "Diamonds 💎", "Clubs 🍀"];
+//      console.log("Card Suits Example:", cardSuits);
+// • The game itself will automatically play using console.log() to display turns, points, cards used, and the outcome of the game. No user input via prompts is required.
+// • The completed project should, when executed, do the following:
+//      1. Deal 26 cards to each player from a deck of 52 cards.
+//      2. Iterate through the turns where each player plays a card.
+//      3. The player who played the higher card is awarded a point.
+//          o Ties result in zero points for both players.
+//      4. After all cards have been played, display the score and declare the winner.
---
+++ tests.js
@@ -0,0 +1,9 @@
+// The following is extra credit (10pts):
+//  • Write a Unit Test using Mocha and Chai for at least one of the functions you write.
+//  • Note Mocha/Chai: The Mocha/Chai framework recently removed Common JS functionality. You will need to use these specific versions:
+//      o chai: "^4.3.10”
+//      o mocha: "^10.2.0”
+//      o You can use this boiler plate project for your unit testing: Mocha/Chai Github Repository
+
+const expect = chai.expect
+const assert = chai.assert
